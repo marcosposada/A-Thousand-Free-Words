ac1953fb8ea2529781a2de637be38c83b3d4d80f
Marcos Posada <citrous@gmail.com>
2014-07-26 15:27:24 +1000

    Icons, Image posts, Archives fix, Footer

--- wp-content/themes/essay/loop.php
+++ wp-content/themes/essay/loop.php
@@ -3,13 +3,13 @@
 	<!-- article -->
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-		<!-- post thumbnail
-		<?php //if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
-			<a href="<?php// the_permalink(); ?>" title="<?php //the_title(); ?>">
-				<?php //the_post_thumbnail(array(120,120)); // Declare pixel size you need inside the array ?>
+		<!-- post thumbnail -->
+		<?php if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
+			<a class="post-thumbnail" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+				<?php the_post_thumbnail('full'); // Declare pixel size you need inside the array ?>
 			</a>
-		<?php //endif; ?>
-		 /post thumbnail -->
+		<?php endif; ?>
+		<!-- /post thumbnail -->
 
 
 		<?php html5wp_excerpt('html5wp_index'); // Build your custom callback length in functions.php ?>
